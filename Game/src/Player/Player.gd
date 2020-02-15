151053683ff6997c3de1b3750702e498e9394d61
Mark Arneman <mark@arneman.me>
2020-02-15 13:06:26 -0500

    Init git, HELLO JAM

---
+++ Game/src/Player/Player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+export var speed := 200
+
+var velocity := Vector2.ZERO
+
+func handle_input():
+    velocity = Vector2.ZERO
+    
+    if Input.is_action_pressed('up'):
+        velocity.y -= 1
+    if Input.is_action_pressed('down'):
+        velocity.y += 1
+    if Input.is_action_pressed('left'):
+        velocity.x -= 1
+    if Input.is_action_pressed('right'):
+        velocity.x += 1        
+    
+    # Normalize so diagonal movement isn't faster
+    velocity = velocity.normalized() * speed
+    
+func _physics_process(_delta):
+    handle_input()
+    velocity = move_and_slide(velocity)
