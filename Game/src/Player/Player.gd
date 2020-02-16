e88aa560231d281bc74298ef1fb3233603d06313
Mark Arneman <mark@arneman.me>
2020-02-16 17:07:11 -0500

    Movement test friction/acceleration

--- Game/src/Player/Player.gd
+++ Game/src/Player/Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
-export var speed := 200
+export (int) var speed = 200
+export (float, 0, 1.0) var friction = 0.1
+export (float, 0, 1.0) var acceleration = 0.25
 
 var velocity := Vector2.ZERO
 
@@ -15,7 +17,7 @@ func handle_input() -> void:
         velocity.x -= 1
     if Input.is_action_pressed('right'):
         velocity.x += 1        
-    
+
     # Normalize so diagonal movement isn't faster
     velocity = velocity.normalized() * speed
     
