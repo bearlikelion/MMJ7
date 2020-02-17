fc9bbc2c38a54cfabf17492f48b9150b90efe737
DaVinci789 <jaden.manacsa1228@gmail.com>
2020-02-16 23:43:05 -0600

    Add player to player group.

--- Game/src/Player/Player.gd
+++ Game/src/Player/Player.gd
@@ -8,10 +8,11 @@ var is_running := false
 var queued_action
 
 func _ready():
-	$states.start()
+	$States.start()
 	pass
 
 func _physics_process(_delta):
+	add_to_group("player")
 	get_input()
 	velocity = move_and_slide(velocity)
 
