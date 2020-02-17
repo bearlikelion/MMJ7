666fc3f16a68b3bc66b024fc73351097f04a2530
BricksParts <bricksparts@gmail.com>
2020-02-17 00:13:27 -0500

    Bricks Level update

--- Game/src/Player/Player.gd
+++ Game/src/Player/Player.gd
@@ -7,20 +7,20 @@ export (float, 0, 1.0) var acceleration = 0.25
 var velocity := Vector2.ZERO
 
 func handle_input() -> void:
-    velocity = Vector2.ZERO
-    
-    if Input.is_action_pressed('up'):
-        velocity.y -= 1
-    if Input.is_action_pressed('down'):
-        velocity.y += 1
-    if Input.is_action_pressed('left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('right'):
-        velocity.x += 1        
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+	if Input.is_action_pressed('left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('right'):
+		velocity.x += 1        
 
-    # Normalize so diagonal movement isn't faster
-    velocity = velocity.normalized() * speed
-    
+	# Normalize so diagonal movement isn't faster
+	velocity = velocity.normalized() * speed
+	
 func _physics_process(_delta : float) -> void:
-    handle_input()
-    velocity = move_and_slide(velocity)
+	handle_input()
+	velocity = move_and_slide(velocity)
