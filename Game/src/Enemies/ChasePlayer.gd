d3d825756cfe5c1b0edd3d499472633c6f2474d6
BricksParts <bricksparts@gmail.com>
2020-02-18 00:42:15 -0500

    Level updates

--- Game/src/Enemies/ChasePlayer.gd
+++ Game/src/Enemies/ChasePlayer.gd
@@ -6,15 +6,15 @@ var velocity = Vector2.ZERO
 var player = null
 
 func _physics_process(_delta) -> void:
-    velocity = Vector2.ZERO
-    if player:
-            velocity = position.direction_to(player.position) * speed
-    velocity = move_and_slide(velocity)
-    
+	velocity = Vector2.ZERO
+	if player:
+			velocity = position.direction_to(player.position) * speed
+	velocity = move_and_slide(velocity)
+	
 
 func _on_AgroRadius_body_entered(body) -> void:
-    if body.is_in_group("player"):
-        player = body
-    
+	if body.is_in_group("player"):
+		player = body
+	
 func _on_AgroRadius_body_exited(_body) -> void:
-    player = null
+	player = null
--- Game/src/Player/Player.gd
+++ Game/src/Player/Player.gd
@@ -5,20 +5,20 @@ export (int) var speed = 200
 var velocity := Vector2.ZERO
 
 func get_input() -> void:
-    velocity = Vector2.ZERO
-    
-    if Input.is_action_pressed('up'):
-        velocity.y -= 1
-    if Input.is_action_pressed('down'):
-        velocity.y += 1
-    if Input.is_action_pressed('left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('right'):
-        velocity.x += 1
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+	if Input.is_action_pressed('left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('right'):
+		velocity.x += 1
 
-    # Normalize so diagonal movement isn't faster
-    velocity = velocity.normalized() * speed
-    
+	# Normalize so diagonal movement isn't faster
+	velocity = velocity.normalized() * speed
+	
 func _physics_process(_delta : float) -> void:
-    get_input()
-    velocity = move_and_slide(velocity)
+	get_input()
+	velocity = move_and_slide(velocity)
