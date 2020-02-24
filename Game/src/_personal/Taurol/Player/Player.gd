c103e0498856341969bec6acf5832680c8358908
BricksParts <bricksparts@gmail.com>
2020-02-24 00:52:34 -0500

    update

--- Game/src/_personal/Taurol/Player/Player.gd
+++ Game/src/_personal/Taurol/Player/Player.gd
@@ -1,6 +1,17 @@
 extends KinematicBody2D
 
 
+<<<<<<< Updated upstream
+=======
+enum WallDirections {
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST,
+   }
+
+
+>>>>>>> Stashed changes
 var look_direction:=Vector2(1,0)
 var motion := Vector2.ZERO
 var rel_velocity :=Vector2.ZERO
