9e24c9fd02029cd9152cb9d2a142e1a2bc2e4dbc
Rinat <rinatsatyl@outlook.com>
2020-04-07 00:57:28 +0600

    Added Mushroom animations

--- Game/src/Enemies/EnemyChase.gd
+++ Game/src/Enemies/EnemyChase.gd
@@ -1,14 +1,33 @@
 extends "res://src/StateMachine/state.gd"
 
+var velMem = Vector2.ZERO
+
 func enter():
 	owner.get_node("AgroRadius").get_node("CollisionShape2D").scale = Vector2(3, 3)
 	
 func exit():
 	owner.get_node("AgroRadius").get_node("CollisionShape2D").scale = Vector2(1 ,1)
+	if velMem.y < 0:
+		owner.get_node("AnimationPlayer").play("Idle_Down")
+	else:
+		owner.get_node("AnimationPlayer").play("Idle_Up")
+	if velMem.x < 0.4:
+		owner.get_node("AnimationPlayer").play("Idle_Left")
+	else:
+		owner.get_node("AnimationPlayer").play("Idle_Right")
 
 func update(delta):
 	owner.velocity = Vector2.ZERO
 	if owner.player:
 		owner.velocity = owner.position.direction_to(owner.player.position) * owner.speed
+		velMem = owner.velocity
+		if owner.velocity.y < 0.2:
+			owner.get_node("AnimationPlayer").play("Run_Down")
+		elif owner.velocity.y > 0.2:
+			owner.get_node("AnimationPlayer").play("Run_Up")
+		elif owner.velocity.x < 0.4:
+			owner.get_node("AnimationPlayer").play("Run_Left")
+		elif owner.velocity.x > 0.4:
+			owner.get_node("AnimationPlayer").play("Run_Right")
 	else:
 		emit_signal("finished","idle")
